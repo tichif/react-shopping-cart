9ad199edfa37c60cfab57714f14621e2aa818030
Dalzon Charles-Hébert <charlby5@gmail.com>
2022-10-31 14:44:41 -0400

    begin with the modal

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,30 @@
+import { useState } from 'react';
 import { Container, Button, Navbar, Modal } from 'react-bootstrap';
 
 const NavbarComponent = () => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
-    <Navbar expand='sm'>
-      <Navbar.Brand href='/'>Ecommerce Store</Navbar.Brand>
-      <Navbar.Toggle />
-      <Navbar.Collapse className='justify-content-end'>
-        <Button>Cart 0 items</Button>
-      </Navbar.Collapse>
-    </Navbar>
+    <>
+      <Navbar expand='sm'>
+        <Navbar.Brand href='/'>Ecommerce Store</Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className='justify-content-end'>
+          <Button onClick={handleShow}>Cart 0 items</Button>
+        </Navbar.Collapse>
+      </Navbar>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Shopping Cart</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <h1>Modal body</h1>
+        </Modal.Body>
+      </Modal>
+    </>
   );
 };
 
